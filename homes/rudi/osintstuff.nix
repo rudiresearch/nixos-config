b932f1254685def67669ff63555d9ad42ef239f8
rudi.research <rudi.research@proton.me>
2025-08-14 16:11:18 +0200

    added exiftool etc.

--- homes/rudi/osintstuff.nix
+++ homes/rudi/osintstuff.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs [
+  home.packages = with pkgs; [
     # security
     clamav
     clamtk
@@ -16,13 +16,11 @@
 
     # privacy
     tor-browser
-    mullvad
     mullvad-vpn
 
 
     # utilities
     bulky
-    gvim
     qbittorrent
     qgis-ltr
     telegram-desktop
